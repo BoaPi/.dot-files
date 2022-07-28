60cf1be5e7334623e63b67573be7ad6faf34fbc0
boapi <tobias.weyer1205@gmail.com>
2022-07-28 22:55:06 +0200

    fix packer implementation

--- nvim/lua/boapi/init.lua
+++ nvim/lua/boapi/init.lua
@@ -1,1 +1,2 @@
 require("boapi.set")
+require("boapi.packer")
--- nvim/lua/boapi/packer.lua
+++ nvim/lua/boapi/packer.lua
@@ -1,8 +1,29 @@
 -- This file can be loaded by calling `lua require('plugins')` from your init.vim
+local vim = vim
+local execute = vim.api.nvim_command
+local fn = vim.fn
+
+-- ensure that packer is installed
+local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+if fn.empty(fn.glob(install_path)) > 0 then
+  execute('!git clone https://github.com/wbthomason/packer.nvim '..install_path)
+  execute 'packadd packer.nvim'
+end
 
 -- Only required if you have packer configured as `opt`
-vim.cmd [[packadd packer.nvim]]
+vim.cmd('packadd packer.nvim')
 
-return require('packer').startup(function()
+local packer = require'packer'
+local util = require'packer.util'
+
+packer.init({
+  package_root = util.join_paths(vim.fn.stdpath('data'), 'site', 'pack')
+})
+
+--- startup and add configure plugins
+packer.startup(function()
+  local use = use
   use 'wbthomason/packer.nvim'
-end)
+  use 'folke/tokyonight.nvim'
+  end
+)
