0096aaddc927b3b3c5fb0486f1a9848423178b31
boapi <tobias.weyer1205@gmail.com>
2022-07-31 22:55:36 +0200

    disable arrow keys

--- nvim/lua/boapi/remap.lua
+++ nvim/lua/boapi/remap.lua
@@ -1,3 +1,11 @@
 local nnoremap = require("boapi.keymap").nnoremap
+local noremap = require("boapi.keymap").noremap
 
+-- quick access of netrw
 nnoremap("<leader>jk", "<CMD>Ex<CR>")
+
+-- disable arrow keys normal, visual and operator-pending mode
+noremap("<up>", "<nop>")
+noremap("<right>", "<nop>")
+noremap("<down>", "<nop>")
+noremap("<left>", "<nop>")
