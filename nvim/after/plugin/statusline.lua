bfaaf012a872689c01179d9c0aa508314498428b
boapi <tobias.weyer1205@gmail.com>
2022-08-27 00:01:21 +0200

    change font to Nerd font and add new separator

--- nvim/after/plugin/statusline.lua
+++ nvim/after/plugin/statusline.lua
@@ -2,7 +2,7 @@ require('lualine').setup {
     options = {
         theme = "gruvbox_dark",
         component_separators = { left = '', right = '|' },
-        section_separators = { left = '', right = ''},
+        section_separators = { left = '', right = ''},
     },
     sections = {
         lualine_a = {'mode'},
