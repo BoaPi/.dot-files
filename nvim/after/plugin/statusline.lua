a757efcb35862cfc3a42b4148b06ad8cbc7ef2e7
boapi <tobias.weyer1205@gmail.com>
2022-08-26 23:40:47 +0200

    add lualine config

---
+++ nvim/after/plugin/statusline.lua
@@ -0,0 +1,15 @@
+require('lualine').setup {
+    options = {
+        theme = "gruvbox_dark",
+        component_separators = { left = '', right = '|' },
+        section_separators = { left = '', right = ''},
+    },
+    sections = {
+        lualine_a = {'mode'},
+        lualine_b = {'branch', 'diff', 'diagnostics'},
+        lualine_c = {'filename'},
+        lualine_x = {'encoding', 'fileformat', 'filetype'},
+        lualine_y = {'windows'},
+        lualine_z = {'location'},
+    },
+}
