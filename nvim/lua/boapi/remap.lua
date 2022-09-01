88ffb451a7d9bb9d964073482c8c732ad528b5fc
boapi <tobias.weyer1205@gmail.com>
2022-09-01 23:35:21 +0200

    add new keybindings

--- nvim/lua/boapi/remap.lua
+++ nvim/lua/boapi/remap.lua
@@ -1,8 +1,18 @@
 local nnoremap = require("boapi.keymap").nnoremap
 local noremap = require("boapi.keymap").noremap
+local telescope = require("telescope.builtin")
 
 -- quick access of netrw
 nnoremap("<leader>jk", "<CMD>Ex<CR>")
+
+-- telescope quickactions
+nnoremap("<leader>ff", "<CMD>Telescope find_files<CR>")
+nnoremap("<leader>gf", "<CMD>Telescope git_files<CR>")
+nnoremap("<leader>gc", "<CMD>Telescope git_commits<CR>")
+nnoremap("<leader>ts", "<CMD>Telescope treesitter<CR>")
+
+-- Neogit quick actions
+nnoremap("<leaderng", "<CMD>Neogit<CR>")
 
 -- disable arrow keys normal, visual and operator-pending mode
 noremap("<up>", "<nop>")
