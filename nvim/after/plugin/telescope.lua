89a99c64ddaa71cfc9236df3e3525f67db1d49d4
boapi <tobias.weyer1205@gmail.com>
2022-09-02 22:47:25 +0200

    show hidden files in file_browser extension

--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -1,6 +1,12 @@
 -- setup telescope
 local telescope = require('telescope')
-telescope.setup{}
+telescope.setup{
+    extensions = {
+		file_browser = {
+			hidden = true,
+		},
+	},
+}
 
 -- load extensions
 telescope.load_extension("file_browser")
