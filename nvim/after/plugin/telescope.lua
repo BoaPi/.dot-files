e6d7e8d1c576fb5df33692a254c7c46c8f9e01bd
boapi <tobias.weyer1205@gmail.com>
2022-09-02 22:27:27 +0200

    remove customization

--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -1,12 +1,6 @@
 -- setup telescope
 local telescope = require('telescope')
-telescope.setup{
-    extensions = {
-        file_browser = {
-             hijack_netrw = true,
-        },
-    },    
-}
+telescope.setup{}
 
 -- load extensions
 telescope.load_extension("file_browser")
