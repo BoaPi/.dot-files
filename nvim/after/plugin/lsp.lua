5c0d74cfcbe11c7564a0473f626a25fac645c0ca
boapi <tobias.weyer1205@gmail.com>
2022-09-03 23:29:40 +0200

    add keybinding for hover info

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -5,6 +5,11 @@ local lspconfig = require'lspconfig'
 local on_attach = function(client, bufnr)
   -- Enable completion triggered by <c-x><c-o>
   vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
+
+  -- Mappings.
+  -- See `:help vim.lsp.*` for documentation on any of the below functions
+  local bufopts = { noremap=true, silent=true, buffer=bufnr }
+  vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
 end
 
 -- different language servers
