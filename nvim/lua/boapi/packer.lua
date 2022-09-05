eb195d7cb994179cd98cf4f0807899b6dc38839a
boapi <tobias.weyer1205@gmail.com>
2022-09-05 16:56:30 +0200

    apply formatting, add harpoon to plugins for file marking

--- nvim/lua/boapi/packer.lua
+++ nvim/lua/boapi/packer.lua
@@ -4,17 +4,17 @@ local execute = vim.api.nvim_command
 local fn = vim.fn
 
 -- ensure that packer is installed
-local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+local install_path = fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
 if fn.empty(fn.glob(install_path)) > 0 then
-  execute('!git clone https://github.com/wbthomason/packer.nvim '..install_path)
+  execute('!git clone https://github.com/wbthomason/packer.nvim ' .. install_path)
   execute 'packadd packer.nvim'
 end
 
 -- Only required if you have packer configured as `opt`
 vim.cmd('packadd packer.nvim')
 
-local packer = require'packer'
-local util = require'packer.util'
+local packer = require 'packer'
+local util = require 'packer.util'
 
 packer.init({
   package_root = util.join_paths(vim.fn.stdpath('data'), 'site', 'pack')
@@ -43,7 +43,10 @@ packer.startup(function()
     run = 'make'
   }
   use 'nvim-telescope/telescope-file-browser.nvim'
-  
+
+  -- harpoon to fix some files
+  use 'ThePrimeagen/harpoon'
+
   -- git related
   use 'TimUntersberger/neogit'
 
@@ -59,5 +62,5 @@ packer.startup(function()
     opt = true
   }
 
-  end
+end
 )
