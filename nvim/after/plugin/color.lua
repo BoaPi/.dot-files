7904178422b380ef9dec4fbd4596718f64077d12
boapi <tobias.weyer1205@gmail.com>
2022-09-10 23:12:03 +0200

    active catppuccin as color scheme, deactivate tokionight

--- nvim/after/plugin/color.lua
+++ nvim/after/plugin/color.lua
@@ -1,5 +1,10 @@
 -- dark background 
 vim.opt.background = "dark"
+vim.g.catppuccin_flavour = "mocha"
 
 -- color scheme tokyonight
-vim.cmd("colorscheme tokyonight")
+-- vim.cmd("colorscheme tokyonight")
+
+-- color theme catppuccin
+require("catppuccin").setup()
+vim.cmd("colorscheme catppuccin")
