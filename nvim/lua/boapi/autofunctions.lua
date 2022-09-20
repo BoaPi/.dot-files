6342e5632cd22f61626705444248851c3c6c600b
boapi <tobias.weyer1205@gmail.com>
2022-09-20 23:33:03 +0200

    remove useless prints, add file name to buf output

--- nvim/lua/boapi/autofunctions.lua
+++ nvim/lua/boapi/autofunctions.lua
@@ -4,18 +4,16 @@ vim.api.nvim_create_autocmd("BufWritePost", {
     group = vim.api.nvim_create_augroup("AutoSaveFormatting", { clear = true }),
     pattern = "main.rs",
     callback = function()
-        print("File saved!!!")
+        vim.api.nvim_buf_set_lines(bufnr, 0, -1, false, { "output of main.rs"})
         vim.fn.jobstart({ "cargo", "run" }, {
             stdout_buffered = true,
             on_stdout = function(_, data)
                 if data then
-                    print(data)
                     vim.api.nvim_buf_set_lines(bufnr, -1, -1, false, data)
                 end
             end,
             on_stderr = function(_, data)
                 if data then
-                    print(data)
                     vim.api.nvim_buf_set_lines(bufnr, -1, -1, false, data)
                 end
             end
