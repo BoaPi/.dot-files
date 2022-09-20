25b0f1d6d7a5269a6c95e65fb82a81e52ab2b8e9
boapi <tobias.weyer1205@gmail.com>
2022-09-20 22:56:11 +0200

    add first draft of auto save run cargo functions

---
+++ nvim/lua/boapi/autofunctions.lua
@@ -0,0 +1,21 @@
+local bufnr = 61
+
+vim.api.nvim_create_autocmd("BufWritePost", {
+    group = vim.api.nvim_create_augroup("AutoSaveFormatting", { clear = true }),
+    pattern = "main.rs",
+    callback = function()
+        print("File saved!!!")
+        vim.api.nvim_buf_set_lines(bufnr, 0, 0, false, {
+            "hello",
+            "world",
+        })
+        vim.fn.jobstart({ "cargo", "run", "main.rs" }, {
+            stdout_buffered = true,
+            on_stdout = function(_, data)
+                if data then
+                    vim.api.nvim_buf_set_lines(bufnr, -1, -1, false, data)
+                end
+            end
+        })
+    end,
+})
