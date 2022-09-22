e46b2ea3e21095e4dbded7678d1720dd2dba86c5
boapi <tobias.weyer1205@gmail.com>
2022-09-22 23:30:51 +0200

    refactor appending to buffer into function

--- nvim/lua/boapi/autofunctions.lua
+++ nvim/lua/boapi/autofunctions.lua
@@ -4,23 +4,24 @@ local rust_commands = {
     fmt = { "cargo", "fmt" },
 }
 
+append_data = function(_, data)
+    if data then
+        vim.api.nvim_buf_set_lines(bufnr, -1, -1, false, data)
+    end
+end
+
+
+-- run "cargo run" after writing to stdout_buffer
+-- getting both outputs from stdout and stderr
 vim.api.nvim_create_autocmd("BufWritePost", {
-    group = vim.api.nvim_create_augroup("AutoSaveFormatting", { clear = true }),
+    group = vim.api.nvim_create_augroup("AutoSaveRun", { clear = true }),
     pattern = "main.rs",
     callback = function()
         vim.api.nvim_buf_set_lines(bufnr, 0, -1, false, { "output of main.rs" })
         vim.fn.jobstart(rust_commands["run"], {
             stdout_buffered = true,
-            on_stdout = function(_, data)
-                if data then
-                    vim.api.nvim_buf_set_lines(bufnr, -1, -1, false, data)
-                end
-            end,
-            on_stderr = function(_, data)
-                if data then
-                    vim.api.nvim_buf_set_lines(bufnr, -1, -1, false, data)
-                end
-            end
+            on_stdout = append_data,
+            on_stderr = append_data,
         })
     end,
 })
