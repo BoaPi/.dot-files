3b0dadde9d51b630f5f0cdd5276e6b56929a2a88
boapi <tobias.weyer1205@gmail.com>
2022-09-22 23:02:58 +0200

    add rust commands table for further automation

--- nvim/lua/boapi/autofunctions.lua
+++ nvim/lua/boapi/autofunctions.lua
@@ -1,10 +1,14 @@
 local bufnr = 61
+local rust_commands = {
+    run = { "cargo", "run" },
+    fmt = { "cargo", "fmt" },
+}
 
 vim.api.nvim_create_autocmd("BufWritePost", {
     group = vim.api.nvim_create_augroup("AutoSaveFormatting", { clear = true }),
     pattern = "main.rs",
     callback = function()
-        vim.api.nvim_buf_set_lines(bufnr, 0, -1, false, { "output of main.rs"})
+        vim.api.nvim_buf_set_lines(bufnr, 0, -1, false, { "output of main.rs" })
         vim.fn.jobstart({ "cargo", "run" }, {
             stdout_buffered = true,
             on_stdout = function(_, data)
