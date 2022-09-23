873ec18eea740df6aeef455485606741cbb84b97
boapi <tobias.weyer1205@gmail.com>
2022-09-23 23:50:54 +0200

    add new custom command to add auto run to files

--- nvim/lua/boapi/autofunctions.lua
+++ nvim/lua/boapi/autofunctions.lua
@@ -22,12 +22,24 @@ local run_main = function(output_bufnr)
         group = vim.api.nvim_create_augroup(name, { clear = true }),
         pattern = "main.rs",
         callback = function()
+            local append_data = function(_, data)
+                if data then
+                    vim.api.nvim_buf_set_lines(output_bufnr, -1, -1, false, data)
+                end
+            end
+
             vim.api.nvim_buf_set_lines(output_bufnr, 0, -1, false, { "output of main.rs" })
             vim.fn.jobstart(rust_commands["run"], {
                 stdout_buffered = true,
-                on_stdout = Append_data,
-                on_stderr = Append_data,
+                on_stdout = append_data,
+                on_stderr = append_data,
             })
         end,
     })
 end
+
+-- add custom user command
+vim.api.nvim_create_user_command("BoaPiRustRun", function()
+    local bufnr = vim.fn.input("Bufnr for main.rs output: ")
+    run_main(tonumber(bufnr))
+end, {})
