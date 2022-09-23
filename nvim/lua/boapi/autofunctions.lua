d1dffa460f7657af54aa165c15209e8230b4751a
boapi <tobias.weyer1205@gmail.com>
2022-09-23 23:23:44 +0200

    encapsulate formatting into funtion to pass in buf nr

--- nvim/lua/boapi/autofunctions.lua
+++ nvim/lua/boapi/autofunctions.lua
@@ -23,15 +23,17 @@ vim.api.nvim_create_autocmd("BufWritePre", {
 
 -- run "cargo run" after writing to stdout_buffer
 -- getting both outputs from stdout and stderr
-vim.api.nvim_create_autocmd("BufWritePost", {
-    group = vim.api.nvim_create_augroup(name, { clear = true }),
-    pattern = "main.rs",
-    callback = function()
-        vim.api.nvim_buf_set_lines(bufnr, 0, -1, false, { "output of main.rs" })
-        vim.fn.jobstart(rust_commands["run"], {
-            stdout_buffered = true,
-            on_stdout = Append_data,
-            on_stderr = Append_data,
-        })
-    end,
-})
+local run_main = function(output_bufnr)
+    vim.api.nvim_create_autocmd("BufWritePost", {
+        group = vim.api.nvim_create_augroup(name, { clear = true }),
+        pattern = "main.rs",
+        callback = function()
+            vim.api.nvim_buf_set_lines(output_bufnr, 0, -1, false, { "output of main.rs" })
+            vim.fn.jobstart(rust_commands["run"], {
+                stdout_buffered = true,
+                on_stdout = Append_data,
+                on_stderr = Append_data,
+            })
+        end,
+    })
+end
