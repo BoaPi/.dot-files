10b59b16005f9be8ace42d3f0b68652fb7048e9c
boapi <tobias.weyer1205@gmail.com>
2022-09-28 15:11:51 +0200

    adjust set cursorlin ehihglight to number

--- nvim/after/plugin/color.lua
+++ nvim/after/plugin/color.lua
@@ -5,6 +5,11 @@ vim.g.catppuccin_flavour = "mocha"
 -- color scheme tokyonight
 -- vim.cmd("colorscheme tokyonight")
 
+-- set cursorline but remove the complete line highlight
+-- only line number
+vim.opt.cursorline = true
+vim.opt.cursorlineopt = "number"
+
 -- color theme catppuccin
 require("catppuccin").setup()
 vim.cmd("colorscheme catppuccin")
