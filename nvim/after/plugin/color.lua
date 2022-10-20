792230629d445eb1b0668c8ec791fdb3b0de37b8
boapi <tobias.weyer1205@gmail.com>
2022-10-21 00:29:18 +0200

    specify tokyo night theme

--- nvim/after/plugin/color.lua
+++ nvim/after/plugin/color.lua
@@ -3,7 +3,7 @@ vim.opt.background = "dark"
 vim.g.catppuccin_flavour = "mocha"
 
 -- color scheme tokyonight
--- vim.cmd("colorscheme tokyonight")
+-- vim.cmd("colorscheme tokyonight-night")
 
 -- set cursorline but remove the complete line highlight
 -- only line number
