cd9137cf3576e8a8dded894633bc85f26c287860
boapi <tobias.weyer1205@gmail.com>
2022-10-25 23:05:35 +0200

    apply formatting updaate statusline

--- nvim/after/plugin/statusline.lua
+++ nvim/after/plugin/statusline.lua
@@ -1,14 +1,14 @@
 require('lualine').setup {
     options = {
         component_separators = { left = '', right = '|' },
-        section_separators = { left = '', right = ''},
+        section_separators = { left = '', right = '' },
     },
     sections = {
-        lualine_a = {'mode'},
-        lualine_b = {'branch', 'diff', 'diagnostics'},
-        lualine_c = {'filename'},
-        lualine_x = {'encoding', 'fileformat', 'filetype'},
-        lualine_y = {'windows'},
-        lualine_z = {'location'},
+        lualine_a = { 'mode' },
+        lualine_b = { 'branch', 'diff', 'diagnostics' },
+        lualine_c = { 'filename' },
+        lualine_x = { 'encoding', 'fileformat', 'filetype' },
+        lualine_y = { { 'tabs', mode = 2 } },
+        lualine_z = { 'location' },
     },
 }
