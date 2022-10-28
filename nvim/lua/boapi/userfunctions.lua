aa0afffd44c658ea322272fe560112fd6ca52a90
boapi <tobias.weyer1205@gmail.com>
2022-10-28 22:57:18 +0200

    add custom user function file

---
+++ nvim/lua/boapi/userfunctions.lua
@@ -0,0 +1,23 @@
+-- custom command
+local auto_functions = require("boapi.autofunctions")
+
+-- RunRust
+-- runs rust code in the given buffer
+vim.api.nvim_create_user_command("RunRust", function()
+    local bufnr = vim.fn.input("Bufnr for main.rs output: ")
+    auto_functions.run_main(tonumber(bufnr))
+end, {})
+
+-- SwitchTab
+-- function to switch between vim tabs quickly
+-- get user input
+-- get current tab count
+-- switch to tab if input is valid and tab present
+vim.api.nvim_create_user_command("SwitchTab", function()
+    local input = tonumber(vim.fn.getcharstr())
+    local tab_count = vim.fn.tabpagenr('$')
+
+    if (input ~= nil and input <= tab_count) then
+        vim.api.nvim_cmd({ cmd = 'tabn', args = { input } }, {})
+    end
+end, {})
