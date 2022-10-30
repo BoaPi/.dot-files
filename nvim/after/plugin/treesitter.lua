0af39ec8ec7a21bd18bc1bb7c55ba8b87902bca8
boapi <tobias.weyer1205@gmail.com>
2022-10-30 22:15:29 +0100

    refactor treesitter setup add new languages

--- nvim/after/plugin/treesitter.lua
+++ nvim/after/plugin/treesitter.lua
@@ -1,10 +1,23 @@
-local configs = require 'nvim-treesitter.configs'
+-- import nvm-treesitter safely
+local setup, configs = pcall(require, "nvim-treesitter.configs")
+
+if not setup then
+  return
+end
+
+-- setup treesitter for all listed languages
+-- and enabling highlighting
 configs.setup {
   ensure_installed = {
     "bash",
     "css",
+    "dockerfile",
+    "gitignore",
     "go",
+    "gomod",
+    "gowork",
     "html",
+    "http",
     "javascript",
     "json",
     "lua",
@@ -14,8 +27,11 @@ configs.setup {
     "regex",
     "rust",
     "scss",
+    "sql",
     "svelte",
     "toml",
+    "tsx",
+    "typescript",
     "yaml"
   },
   highlight = {
