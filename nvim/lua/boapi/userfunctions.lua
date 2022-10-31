e18232ccae6a9d82b9fa1166bce8d9ee261d1917
boapi <tobias.weyer1205@gmail.com>
2022-10-31 23:28:07 +0100

    fix user cmd RunRust, command gets bufnr automatically

--- nvim/lua/boapi/userfunctions.lua
+++ nvim/lua/boapi/userfunctions.lua
@@ -5,16 +5,7 @@ local auto_functions = require("boapi.autofunctions")
 -- runs rust code in the given buffer
 vim.api.nvim_create_user_command("RunRust", function()
     vim.api.nvim_cmd({ cmd = "vnew" }, {})
-    -- local echo_bufnr = vim.api.nvim_cmd({
-    --     cmd = "echo",
-    --     args = { "bufnr()" }
-    -- },
-    --     {})
-    -- print("Test", echo_bufnr)
-    local test = vim.api.vim_buf_get_name({ buffer = 0 })
-    print(test)
-
-    local bufnr = vim.fn.input("Bufnr for main.rs output: ")
+    local bufnr = vim.api.nvim_get_current_buf()
     auto_functions.run_main(tonumber(bufnr))
 end, {})
 
