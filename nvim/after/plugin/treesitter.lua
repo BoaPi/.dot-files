5da99dc59b2efcd84359d1a415c6afff5d259db1
boapi <tobias.weyer1205@gmail.com>
2023-04-25 22:22:54 +0200

    setup tree-sitter playground

--- nvim/after/plugin/treesitter.lua
+++ nvim/after/plugin/treesitter.lua
@@ -47,7 +47,33 @@ configs.setup({
   indent = {
     enable = true,
   },
+  query_linter = {
+    enable = true,
+    use_virtual_text = true,
+    lint_events = { "BufWrite", "CursorHold" },
+  },
 })
 
 -- adjust highlight to capture groups
-vim.api.nvim_set_hl(0, "@text.todo", {})
+--vim.api.nvim_set_hl(0, "@text.todo", {})
+
+treesitter_playground.setup({
+  playground = {
+    enable = true,
+    disable = {},
+    updatetime = 25, -- Debounced time for highlighting nodes in the playground from source code
+    persist_queries = false, -- Whether the query persists across vim sessions
+    keybindings = {
+      toggle_query_editor = "o",
+      toggle_hl_groups = "i",
+      toggle_injected_languages = "t",
+      toggle_anonymous_nodes = "a",
+      toggle_language_display = "I",
+      focus_language = "f",
+      unfocus_language = "F",
+      update = "R",
+      goto_node = "<cr>",
+      show_help = "?",
+    },
+  },
+})
