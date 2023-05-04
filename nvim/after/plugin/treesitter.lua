2a7c54080065ff941a4fc77686d49c77afc9fea6
Tobias Weyer <tobias.weyer1205@gmail.com>
2023-05-04 13:00:58 +0200

    Update treesitter.lua

--- nvim/after/plugin/treesitter.lua
+++ nvim/after/plugin/treesitter.lua
@@ -1,12 +1,7 @@
 -- import nvm-treesitter safely
 local treesitter_status, configs = pcall(require, "nvim-treesitter.configs")
-local treesitter_playground_status, treesitter_playground = pcall(require, "nvim-treesitter/playground")
 
 if not treesitter_status then
-  return
-end
-
-if not treesitter_playground_status then
   return
 end
 
@@ -18,9 +13,6 @@ configs.setup({
     "css",
     "dockerfile",
     "gitignore",
-    "go",
-    "gomod",
-    "gowork",
     "graphql",
     "html",
     "http",
@@ -40,40 +32,15 @@ configs.setup({
     "typescript",
     "yaml",
   },
+  sync_install = false,
+  auto_install = true,
   highlight = {
     enable = true,
-  },
-  -- Indentation based on treesitter for the = operator
-  indent = {
-    enable = true,
+    additional_vim_regex_highlighting = false,
   },
   query_linter = {
     enable = true,
     use_virtual_text = true,
     lint_events = { "BufWrite", "CursorHold" },
-  },
-})
-
--- adjust highlight to capture groups
---vim.api.nvim_set_hl(0, "@text.todo", {})
-
-treesitter_playground.setup({
-  playground = {
-    enable = true,
-    disable = {},
-    updatetime = 25, -- Debounced time for highlighting nodes in the playground from source code
-    persist_queries = false, -- Whether the query persists across vim sessions
-    keybindings = {
-      toggle_query_editor = "o",
-      toggle_hl_groups = "i",
-      toggle_injected_languages = "t",
-      toggle_anonymous_nodes = "a",
-      toggle_language_display = "I",
-      focus_language = "f",
-      unfocus_language = "F",
-      update = "R",
-      goto_node = "<cr>",
-      show_help = "?",
-    },
   },
 })
