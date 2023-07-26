b1de5b15d0c52b60ca4d42c993a1f8f771449bd1
Tobias Weyer <tobias.weyer1205@gmail.com>
2023-07-26 17:29:19 +0200

    Update lspconfig.lua

--- nvim/after/plugin/lsp/lspconfig.lua
+++ nvim/after/plugin/lsp/lspconfig.lua
@@ -1,95 +1,106 @@
--- import neodev plugin safely
+-- import packages plugin safely
 local neodev_status, neodev = pcall(require, "neodev")
 if not neodev_status then
   return
 end
 neodev.setup({})
 
--- import lspconfig plugin safely
-local lspconfig_status, lspconfig = pcall(require, "lspconfig")
-if not lspconfig_status then
+local lsp_status, lsp = pcall(require, "lsp-zero")
+if not lsp_status then
+  return
+end
+lsp.preset({})
+
+local mason_status, mason = pcall(require, "mason")
+if not mason_status then
   return
 end
 
--- import cmp-nvim-lsp plugin safely
-local cmp_nvim_lsp_status, cmp_nvim_lsp = pcall(require, "cmp_nvim_lsp")
-if not cmp_nvim_lsp_status then
+local mason_lspconfig_status, mason_lspconfig = pcall(require, "mason-lspconfig")
+if not mason_lspconfig_status then
   return
 end
 
--- Add additional capabilities supported by nvim-cmp
-local client_capabilities = vim.lsp.protocol.make_client_capabilities()
-local capabilities =
-  vim.tbl_deep_extend("force", client_capabilities, cmp_nvim_lsp.default_capabilities(client_capabilities))
-
--- NOTE: on_attach()
--- will enable listed key mappings only when an lsp is attached
--- to the buffer
-local on_attach = function(_, bufnr)
-  -- NOTE: plugins like rust-tools & mason
-  -- will bring custom functions with them. In here we can attach them
-  -- to the buffers which needs them, e.g.
-  -- rust-tool mappings only for the rust-lsp
-  local bufopts = { noremap = true, silent = true, buffer = bufnr }
-  vim.keymap.set("n", "K", vim.lsp.buf.hover, bufopts)
-  vim.keymap.set("n", "<leader>f", vim.lsp.buf.format, bufopts)
+local mason_null_ls_status, mason_null_ls = pcall(require, "mason-null-ls")
+if not mason_null_ls_status then
+  return
 end
 
--- different language servers
--- rust
-lspconfig.rust_analyzer.setup({
-  on_attach = on_attach,
-  capabilities = capabilities,
-})
+local cmp_status, cmp = pcall(require, "cmp")
+if not cmp_status then
+  return  
+end
 
--- lua
-lspconfig.lua_ls.setup({
-  on_attach = on_attach,
-  capabilities = capabilities,
-  settings = {
-    Lua = {
-      runtime = {
-        -- used Lua version
-        version = "LuaJIT",
-      },
-      completion = {
-        callSnipper = "Replace",
-      },
-      diagnostics = {
-        -- Get the language server to recognize the `vim` global
-        globals = { "vim" },
-      },
-      workspace = {
-        checkThirdParty = false,
-      },
+local cmp_action = lsp.cmp_action()
+
+-- setup
+lsp.on_attach(function(_, bufnr)
+  lsp.default_keymaps({ buffer = bufnr })
+end)
+
+-- setup auto-complete
+lsp.extend_cmp()
+
+require('luasnip.loaders.from_vscode').lazy_load()
+
+cmp.setup({
+  sources = {
+      { name = "buffer"},
+      { name = "nvim_lsp" },
+      { name = "nvim_lua"},
+      { name = "luasnip" },
     },
+  mapping = {
+    ['<C-f>'] = cmp_action.luasnip_jump_forward(),
+    ['<C-b>'] = cmp_action.luasnip_jump_backward(),
+  },
+  preselect = 'item',
+  completion = {
+    completeopt = 'menu,menuone,noinsert'
   },
 })
 
---JSON
-lspconfig.jsonls.setup({
-  settings = {
-    json = {
-      schemas = require("schemastore").json.schemas(),
-      validate = { enable = true },
-    },
+lsp.set_sign_icons({
+  error = '✘',
+  warn = '▲',
+  hint = '⚑',
+  info = '»'
+})
+
+mason.setup({})
+mason_lspconfig.setup({
+  -- Replace the language servers listed here
+  -- with the ones you want to install
+  ensure_installed = {
+    "cssls",
+    "denols",
+    "eslint",
+    "html",
+    "jsonls",
+    "lua_ls",
+    "rust_analyzer",
+    "stylelint_lsp",
+    "svelte",
+    "tsserver",
+  },
+  automatic_installation = true,
+  handlers = {
+    lsp.default_setup,
+    lua_ls = function()
+      -- (Optional) Configure lua language server for neovim
+      require('lspconfig').lua_ls.setup(lsp.nvim_lua_ls())
+    end,
   },
 })
 
--- deno / typescript lsps
--- deno
-lspconfig.denols.setup({
-  on_attach = on_attach,
-  init_options = {
-    lint = true,
+mason_null_ls.setup({
+  -- list of formatters & linters for mason to install
+  ensure_installed = {
+    "eslint_d",  -- for all projects using eslint
+    "prettierd", -- for all project using not eslint
+    "stylua",    -- lua formatter
+    "stylelint", -- css linter
   },
-  single_file_support = false,
-})
--- typescript
-lspconfig.tsserver.setup({})
-
--- cssls
-lspconfig.cssls.setup({
-  on_attach = on_attach,
-  capabilities = capabilities,
+  -- auto-install configured formatters & linters (with null-ls)
+  automatic_installation = true,
 })
