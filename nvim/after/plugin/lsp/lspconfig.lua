d6a7bf196a12180665284a610f36ce81f811aa3d
Tobias Weyer <tobias.weyer1205@gmail.com>
2024-02-01 22:28:44 +0100

    Update lspconfig.lua

--- nvim/after/plugin/lsp/lspconfig.lua
+++ nvim/after/plugin/lsp/lspconfig.lua
@@ -12,23 +12,8 @@ end
 local lsp = lsp_init.preset({})
 local cmp_action = lsp.cmp_action()
 
-local mason_status, mason = pcall(require, "mason")
-if not mason_status then
-  return
-end
-
 local lspconfig_status, lspconfig = pcall(require, "lspconfig")
 if not lspconfig_status then
-  return
-end
-
-local mason_lspconfig_status, mason_lspconfig = pcall(require, "mason-lspconfig")
-if not mason_lspconfig_status then
-  return
-end
-
-local mason_null_ls_status, mason_null_ls = pcall(require, "mason-null-ls")
-if not mason_null_ls_status then
   return
 end
 
@@ -72,57 +57,4 @@ lsp.set_sign_icons({
   warn = "▲",
   hint = "⚑",
   info = "»",
-})
-
-mason.setup({})
-mason_lspconfig.setup({
-  -- Replace the language servers listed here
-  -- with the ones you want to install
-  ensure_installed = {
-    "cssls",
-    "denols",
-    "eslint",
-    "html",
-    "jsonls",
-    "lua_ls",
-    "gopls",
-    "stylelint_lsp",
-    "svelte",
-    "tailwindcss",
-    "tsserver",
-  },
-  automatic_installation = true,
-  handlers = {
-    lsp.default_setup,
-    lua_ls = function()
-      lspconfig.lua_ls.setup(lsp.nvim_lua_ls({
-        on_attach = function()
-          vim.keymap.set("n", "<leader>f", function()
-            stylua.format()
-          end)
-        end,
-      }))
-    end,
-    svelte = function()
-      lspconfig.svelte.setup({
-        on_attach = function()
-          vim.keymap.set("n", "<leader>f", function()
-            vim.lsp.buf.format()
-          end)
-        end,
-      })
-    end,
-  },
-})
-
-mason_null_ls.setup({
-  -- list of formatters & linters for mason to install
-  ensure_installed = {
-    "eslint_d", -- for all projects using eslint
-    "prettierd", -- for all project using not eslint
-    "stylua", -- lua formatter
-    "stylelint", -- css linter
-  },
-  -- auto-install configured formatters & linters (with null-ls)
-  automatic_installation = true,
 })
