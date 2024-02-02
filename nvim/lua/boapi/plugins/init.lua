f866bbaa4bc3024d407f7c385713a992866fa904
boapi <tobias.weyer1205@gmail.com>
2024-02-02 23:16:35 +0100

    add new plugins folder with init of standard plugins

---
+++ nvim/lua/boapi/plugins/init.lua
@@ -0,0 +1,6 @@
+return {
+  "nvim-lua/plenary.nvim", -- lua functions that many plugins use
+  "kdheepak/lazygit.nvim", -- lazy git implementation in nvim
+  "mbbill/undotree", -- show undo tree on the site
+  -- TODO: enable plugin  use("kylechui/nvim-surround") -- add, delete, change surroundings
+}
