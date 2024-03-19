3569be4b86b09d3062cdb17d44a012018bc3e84b
Tobias Weyer <tobias.weyer1205@gmail.com>
2024-03-19 17:05:33 +0100

    add task-toggler

--- nvim/lua/boapi/plugins/init.lua
+++ nvim/lua/boapi/plugins/init.lua
@@ -3,4 +3,11 @@ return {
   "kdheepak/lazygit.nvim", -- lazy git implementation in nvim
   "mbbill/undotree", -- show undo tree on the site
   -- TODO: enable plugin  use("kylechui/nvim-surround") -- add, delete, change surroundings
+
+  {
+    "BoaPi/task-toggler.nvim",
+    config = true,
+    ft = "markdown",
+    dependencies = "nvim-treesitter",
+  },
 }
