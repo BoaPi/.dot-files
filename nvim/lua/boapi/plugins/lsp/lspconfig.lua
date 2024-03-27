c513bf84fa49cb36b582d591de86fdfff5caaae1
Tobias Weyer <tobias.weyer1205@gmail.com>
2024-03-27 16:22:05 +0100

    Update lspconfig.lua

--- nvim/lua/boapi/plugins/lsp/lspconfig.lua
+++ nvim/lua/boapi/plugins/lsp/lspconfig.lua
@@ -4,21 +4,30 @@ return {
   dependencies = {
     "hrsh7th/cmp-nvim-lsp",
     "VonHeikemen/lsp-zero.nvim",
+    "hrsh7th/nvim-cmp", -- auto-completion
+    "hrsh7th/cmp-buffer", -- source for text in buffer
+    "hrsh7th/cmp-path", -- source for file system paths
+    "hrsh7th/cmp-nvim-lua", -- nvim lua API
+    "L3MON4D3/LuaSnip", -- snippet engine
+    "saadparwaiz1/cmp_luasnip", -- for autocompletion
+    "rafamadriz/friendly-snippets", -- useful snippets
+    "onsails/lspkind.nvim", -- vs-code like pictograms
   },
   config = function()
     local lspconfig = require("lspconfig")
-
     local cmp_nvim_lsp = require("cmp_nvim_lsp")
-
     local lsp_init = require("lsp-zero")
+    local cmp = require("cmp")
+    local lspkind = require("lspkind")
+    local cmp_action = lsp_init.cmp_action()
 
     local lsp = lsp_init.preset({})
 
-    -- setup
+    -- setup LSP
     lsp.on_attach(function(_, bufnr)
       lsp.default_keymaps({ buffer = bufnr })
 
-      vim.keymap.set('n', '<leader>ca', '<cmd>lua vim.lsp.buf.code_action()<cr>', {buffer = bufnr})
+      vim.keymap.set("n", "<leader>ca", "<cmd>lua vim.lsp.buf.code_action()<cr>", { buffer = bufnr })
     end)
     lsp.extend_cmp()
     lsp.capabilities = cmp_nvim_lsp.default_capabilities()
@@ -38,5 +47,37 @@ return {
 
     -- Templ LSP
     lspconfig.templ.setup({})
+
+    -- HTML LSP
+    lspconfig.html.setup({})
+
+    -- loads vscode style snippets from installed plugins (e.g. friendly-snippets)
+    require("luasnip.loaders.from_vscode").lazy_load()
+
+    -- setup auto-completion
+    cmp.setup({
+      sources = {
+        { name = "buffer" },
+        { name = "nvim_lsp" },
+        { name = "nvim_lua" },
+        { name = "luasnip" },
+        { name = "path" },
+      },
+      preselect = "item",
+      completion = {
+        completeopt = "menu,menuone,noinsert",
+      },
+      -- configure lspkind for vs-code like pictograms in completion menu
+      formatting = {
+        format = lspkind.cmp_format({
+          maxwidth = 50,
+          ellipsis_char = "...",
+        }),
+      },
+      mapping = {
+        ["<C-f>"] = cmp_action.luasnip_jump_forward(),
+        ["<C-b>"] = cmp_action.luasnip_jump_backward(),
+      },
+    })
   end,
 }
