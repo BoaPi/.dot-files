45309a8700644535d95aed776b6ef8012ba4f6ad
boapi <tobias.weyer1205@gmail.com>
2026-04-15 21:42:35 +0200

    delete old nvim config setup

--- nvim/lua/boapi/core/globals.lua
+++
@@ -1,5 +0,0 @@
--- print content of values and not only references
-P = function(v)
-  print(vim.inspect(v))
-  return v
-end
--- nvim/lua/boapi/core/init.lua
+++
@@ -1,4 +0,0 @@
-require("boapi.core.globals")
-require("boapi.core.set")
-require("boapi.core.remap")
-require("boapi.core.keymap")
