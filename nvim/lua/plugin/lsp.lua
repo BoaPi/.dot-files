406f70aea71cb1deb76e7b7b674d1651aa3d6e67
Tobias Weyer <tobias.weyer1205@gmail.com>
2026-07-17 15:54:20 +0200

    Configure LSP clients with snippet capabilities
    Added configuration for LSP clients with snippet support.

---
+++ nvim/lua/plugin/lsp.lua
@@ -0,0 +1,24 @@
+--Enable (broadcasting) snippet capability for completion
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities.textDocument.completion.completionItem.snippetSupport = true
+
+vim.lsp.config("cssls", {
+  capabilities = capabilities,
+})
+vim.lsp.enable("cssls")
+
+vim.lsp.enable("eslint")
+vim.lsp.enable("gopls")
+
+-- lua setup for development
+vim.lsp.config("lua_ls", {
+  settings = {
+    Lua = {
+      diagnostics = { globals = { "vim" } },
+    },
+  },
+})
+vim.lsp.enable("lua_ls")
+
+vim.lsp.enable("oxlint")
+vim.lsp.enable("ts_ls")
